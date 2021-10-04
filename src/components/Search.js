b1b5376c739bf21d9383d2950e18211de70f4902
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-10-04 17:34:12 -0400

    wikipedia setup axios call, list mapeed and rendered

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    }
+    if (term) {
+      fetchData();
+    }
+  }, [term]);
+
+  const renderedResults = results.map(({ title, snippet, pageid }) => {
+    return (
+      <div key={pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${pageid}`}
+            className="ui button"
+          >
+            Go to article
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{title}</div>
+          {snippet.replaceAll(/<span class="searchmatch">|<\/?span>/g, "")}
+        </div>
+      </div>
+    );
+  });
   return (
     <div>
-      <h1>search</h1>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            type="text"
+            className="input"
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
