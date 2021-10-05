43518d2af7a1e2eeb29fcd7e792a1d80f791cc7b
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-10-05 18:40:55 -0400

    debounced convert search term

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,17 +2,40 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
+  const [debouncedText, setDebouncedText] = useState(text);
+
   useEffect(() => {
-    console.log("new lang or text");
-  }, [language, text]);
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const results = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: debouncedText,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+      setTranslated(results.data.data.translations[0].translatedText);
+    };
+    fetchData();
+  }, [language, debouncedText]);
 
   return (
     <div>
-      <h1>
-        {language.label}
-        <br />
-        {text}
-      </h1>
+      <h1 className="ui header">{translated}</h1>
     </div>
   );
 };
